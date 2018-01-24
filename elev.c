7534363a55c1e0a6f883a4a23170a705a7f151b6
ruchitom <35732783+ruchitom@users.noreply.github.com>
2018-01-24 23:36:25 +0530

    Add files via upload

---
+++ elev.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int n,m,r;
+	printf("enter a no.");
+	scanf("%d",&n);
+	printf("enter the power");
+	scanf("%d",&m);
+	r=pow(n,m);
+	printf("result is %d",r);
+	
+}
