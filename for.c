e2a96e0732040f97c78b3cb76d2c61c001cc8698
ruchitom <35732783+ruchitom@users.noreply.github.com>
2018-01-25 21:40:41 +0530

    Update for.c

--- for.c
+++ for.c
@@ -2,13 +2,11 @@
 void main()
 {
 	int n,m,i;
-	printf("enter stat value\n");
-	scanf("%d",&n);
-	printf("enter end value\n");
-	scanf("%d",&m);
+	printf("enter stat value and end value\n");
+	scanf("%d%d",&n,&m);
 	for(i=n+1;i<=m;i++)
 	{
 		if(i%2!=0)	
-			printf("the result is %d",i);
+			printf("%d ",i);
 	}
 }
